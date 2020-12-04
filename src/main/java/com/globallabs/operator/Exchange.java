511292f80a9f11d546ea633328c3578b74e25ed2
Daniel Rodriguez <danielrs9705@gmail.com>
2020-12-04 16:00:25 +0100

    Add class comment for the Exchange class

--- src/main/java/com/globallabs/operator/Exchange.java
+++ src/main/java/com/globallabs/operator/Exchange.java
@@ -9,7 +9,43 @@ import com.globallabs.telephone.Status;
 import com.globallabs.telephone.Telephone;
 import java.util.LinkedList;
 
-
+/**
+ * Exchange is the class that manages the communication between
+ * Telephones. Its principal goal of all its functionalities 
+ * is to synchronize the calls between Telephones. 
+ * Between these functionalities are:
+ * <ul>
+ * <li>Create a line of communication between Telephones.
+ * <li>If a Telephone t1 accept a call from a Telephone t2 then t1
+ *     "open" the call
+ * <li>"Close" a communication when one of the peers end the call
+ * <li>Keeps a list of all the Telephones belonging to the
+ *     exchange
+ * </ul>
+ *  
+ * <p>One remark to keep mind when using Exchange is that
+ * its three communication management functions (enrouteCall, openCallBetween
+ * and closeCallBetween) work as a pipeline. To make a full call
+ * process (dial -> call -> hang up) you have to use the functions in the
+ * following order:
+ * 1) enrouteCall (Made by the Telephone dialing)
+ * 2) openCallBetween (Made by the Telephone receiving the call)
+ * 3) closeCallBetween (Made by the Telephone who hang up the call)
+ * 
+ * <p>Second remark for all the three communication functions the order
+ * of the parameters is important. These parameters follow this logic:
+ * the first parameter represents the Telephone who called the function,
+ * the second parameter represents the Telephone that is going to be
+ * synchronized by the consequences of the function. For example:
+ * 
+ * <p>You have two telephones t1 and t2, t1 dial t2, this means that t1
+ * called the function enrouteCall. Then, enrouteCall will synchronize t2
+ * to give it the information that t1 is calling him. All the other functions
+ * work in the same way.
+ * 
+ * @author Daniel RODRIGUEZ
+ * @since 1.0
+ */
 public class Exchange implements ExchangeSpecification {
     
   private LinkedList<Telephone> telephones;
