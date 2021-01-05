af4a7b3d8ce8a954e536d559f479e22d68108c59
byronbark <byronbarkhuizen@gmail.com>
2021-01-05 14:23:06 +0100

    Telephone javadoc additional instructions about state of the phone

--- src/main/java/com/globallabs/telephone/package-info.java
+++ src/main/java/com/globallabs/telephone/package-info.java
@@ -8,5 +8,10 @@
  *
  * @since 1.0
  * @author Byron Barkhuizen
+ * 
+ * <p>Some important points to consider for the use and operation of the tests are;
+ * 1) lastCall can be null if the phone is being initialized, or has not been used
+ * 2) incomingCall can be null if the host phone is not being called
+ * 
  */
 package com.globallabs.telephone;
