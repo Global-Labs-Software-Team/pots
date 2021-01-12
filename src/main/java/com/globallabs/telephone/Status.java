74621f4244a0e5c4205a613b3d0b085d78ab973d
Daniel Rodriguez <danielrs9705@gmail.com>
2021-01-12 10:15:57 +0100

    Add documentation for status

--- src/main/java/com/globallabs/telephone/Status.java
+++ src/main/java/com/globallabs/telephone/Status.java
@@ -2,6 +2,12 @@ package com.globallabs.telephone;
 
 /**
  * The different status that a phone can have.
+ * <ul>
+ * <li> BUSY: When the phone is in a call with another phone.
+ * <li> OFF_CALL: When the phone is free.
+ * <li> RINGING: When is receiving a call from another peer.
+ * <li> DIALING: When the phone is dialing another phone.
+ * </ul>
  *
  * @author danielrs
  */
