44bc5e1d916ea4b92bab4d68060de41c5235ca31
byronbark <byronbarkhuizen@gmail.com>
2021-01-20 13:19:14 +0100

    First commit for caller id feature, class creation

---
+++ src/main/java/com/globallabs/telephone/TelephoneCallerID.java
@@ -0,0 +1,29 @@
+package com.globallabs.telephone;
+
+import com.globallabs.operator.Exchange;
+import com.globallabs.operator.Pipeline;
+import com.globallabs.phonedata.TelephoneModel;
+import com.globallabs.phoneexceptions.PhoneExistInNetworkException;
+import com.globallabs.phoneexceptions.PhoneNotFoundException;
+
+public class TelephoneCallerID extends TelephoneWithPipeline {
+
+    private int incoming;
+    private TelephoneWithPipeline phone;
+    private int information;
+    private Exchange exchange;
+
+    public TelephoneCallerID(TelephoneModel phoneInfo, Exchange exchange, Pipeline publishPipe)
+            throws PhoneExistInNetworkException {
+        super(phoneInfo, exchange, publishPipe);
+        this.exchange = exchange;
+    }
+    
+    public int findCallerID(TelephoneWithPipeline incomingPhone)
+            throws PhoneNotFoundException {
+        incoming = incomingPhone.getIncomingCall();
+        phone = exchange.getPhone(incoming);
+        information = phone.getTelephoneId();
+        return information;
+    }
+}
