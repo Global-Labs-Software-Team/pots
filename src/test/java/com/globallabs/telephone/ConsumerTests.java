51514341af17cc7a552543557b2c593b22f1d3ad
Daniel Rodriguez <danielrs9705@gmail.com>
2021-02-04 18:21:27 +0100

    Fix bug in the consumer tests

--- src/test/java/com/globallabs/telephone/ConsumerTests.java
+++ src/test/java/com/globallabs/telephone/ConsumerTests.java
@@ -7,6 +7,7 @@ import com.globallabs.operator.Pipeline;
 import com.globallabs.phonedata.TelephoneModel;
 import com.globallabs.phoneexceptions.InvalidNumberException;
 import com.globallabs.phoneexceptions.PhoneExistInNetworkException;
+import java.util.LinkedList;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -14,27 +15,31 @@ public class ConsumerTests {
 
   public static String name;
   public static Pipeline toConsume = new Pipeline("consumerPipe");
-
+  public static LinkedList<Integer> stream;
+  public static int bitExpected;
   private static int ID1 = 1;
 
   private static Consumer consumerOne;
-  private static Exchange exchange;
+  private static Exchange exchange = Exchange.getInstance();
   private static TelephoneWithPipeline telephoneOne;
-  private static Pipeline q;
 
   /**
-    * Set up all the necessary functionality for the tests. Each time
-    * a test is executed a consume pipe and linkedlist of stream data is provided.
-    * 
-    */
+   * Set up all the necessary functionality for the tests. Each time
+   * a test is executed a consume pipe and linkedlist of stream data is provided.
+   * 
+   */
   @BeforeAll
   public static void setUp() throws InvalidNumberException, PhoneExistInNetworkException {
     name = "consumer";
-    toConsume = new Pipeline("consumerPipe");
-    telephoneOne = (TelephoneWithPipeline) new TelephoneWithPipeline(new TelephoneModel(ID1), 
-        exchange);
-    q = telephoneOne.getConsumePipe();
-    consumerOne = new Consumer(name, q, telephoneOne);
+    bitExpected = 0;
+    stream = new LinkedList<Integer>();
+    stream.add(bitExpected);
+    stream.add(1);
+    stream.add(0); // Stream form by 010
+    toConsume = new Pipeline("consumerPipe", stream);
+    telephoneOne = new TelephoneWithPipelineForTests(new TelephoneModel(ID1), 
+        exchange, stream);
+    consumerOne = new Consumer(name, toConsume, telephoneOne);
   }
   
   /**
@@ -48,14 +53,13 @@ public class ConsumerTests {
   }
   
   /**
-     * Verifies the receiverBit method works by checking the length of the consume pipeline.
-    * It verifies all bits have been consumed.
-    * (see {@link com.globallabs.phonedata.telephone.Consumer}).
-    */
-  
+  * Verifies the receiverBit method works by checking the length of the consume pipeline.
+  * It verifies all bits have been consumed.
+  * (see {@link com.globallabs.phonedata.telephone.Consumer}).
+  */
   @Test
   public void testConsume() {
     consumerOne.receiveBit();
-    // AssertsEquals(0, toConsume.length);
+    assertEquals(bitExpected, consumerOne.getBitsReceived().get(0));
   }
 }
